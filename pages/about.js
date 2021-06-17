87f64aa2810c62a612e5451a572e811f3f75fbbb
Alex <alex@localhost.localdomain>
2021-06-17 08:08:00 +0300

    load foto MobileAnalize

--- pages/about.js
+++ pages/about.js
@@ -8,11 +8,12 @@ const About = () => {
             <h3 className="title">ABOUT US</h3>
             <div className={styles.about_wrap}>
                <article className={styles.about_tx}>
-                  <p>Our company has 3 years experience on the USA market.
-                  We specialize in creating additional sales and increasing
-                  conversions through listing optimizations across multiple platforms.
-                  Our in-house staff has experience in professional product photography, 
-                  photo editing, and writing copy that converts.</p>
+                  <p>Our company has four years' experience in the USA market.
+                     We specialize in creating additional sales and increasing
+                     conversions through listing optimizations across multiple platforms (Shopify, Amazon).  
+                     We sell products on our eCommerce store.
+                     Our in-house staff has experience in professional product photography, 
+                     photo editing, and writing copy that converts.</p>
                               <p>Recent events show how rapidly everything is changing in the world of e-commerce.
                   And what worked a year ago does not bring the desired result at present.
                   We carefully monitor new changes in e-commerce and try to accept them in our work.</p>
